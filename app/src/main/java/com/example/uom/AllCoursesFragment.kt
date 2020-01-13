65036e30a3723f347be1573be3c8fc291f7d52fc
matzi24 <ggeomat@gmail.com>
2020-01-13 18:42:35 +0200

    Added Fragments

---
+++ app/src/main/java/com/example/uom/AllCoursesFragment.kt
@@ -0,0 +1,156 @@
+package com.example.uom
+
+
+import android.accounts.AccountManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.CookieManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
+
+class AllCoursesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_all_courses, container, false)
+
+
+        val usrText = getActivity()!!.findViewById<EditText>(R.id.username_textview)
+        val passwdText = getActivity()!!.findViewById<EditText>(R.id.password_textview)
+        val userText = getActivity()!!.findViewById<TextView>(R.id.user_text)
+        val loginButton = getActivity()!!.findViewById<Button>(R.id.login_button)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view2)
+        //val webView = findViewById<WebView>(R.id.webView2)
+
+        suspend fun login(username: String? ,password: String?): String? {
+            val url = "https://compus.uom.gr/modules/auth/login.php"
+            return GlobalScope.async(Dispatchers.IO) {
+                var cookie: String? = null
+                try {
+                    val response = Jsoup.connect(url)
+                            .data("uname", username)
+                            .data("pass", password)
+                            .data("login", "submit")
+                            .method(Connection.Method.POST)
+                            .execute()
+
+                    cookie = if (response.body().contains("Τα Μαθήματά Μου")) response.cookie("PHPSESSID") else null
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                return@async cookie
+            }.await()
+        }
+
+        suspend fun fetchHome(cookie: String?): Document? {
+            val url = "https://compus.uom.gr/index.php"
+            var document: Document? = null
+            return GlobalScope.async(Dispatchers.IO) {
+                try {
+                    val response = Jsoup.connect(url)
+                            .cookie("PHPSESSID",cookie)
+                            .method(Connection.Method.GET)
+                            .execute()
+                    document = response.parse()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                return@async document
+            }.await()
+        }
+
+        fun showHome(document: Document): ArrayList<Course> {
+            //Show User Name
+            val user = document.select("td[class=info_user]").text()
+            userText.text = user
+
+            //Parse Courses
+            val courses = ArrayList<Course>()
+            val coursesElements = document.select("td[class=external_table]")
+            for (i in coursesElements.indices) courses.add(Course(coursesElements[i]))
+
+            //Display Courses
+            val adapter = CourseAdapter(context, R.layout.course_item, courses)
+            val linearLayoutManager = LinearLayoutManager(context)
+            recyclerView.layoutManager = linearLayoutManager
+            recyclerView.adapter = adapter
+
+            return courses
+        }
+
+        suspend fun getAnnouncements(cookie: String,courses: ArrayList<Course>){
+            return GlobalScope.async(Dispatchers.IO) {
+                try {
+                    Jsoup.connect(courses[6].url)
+                            .cookie("PHPSESSID",cookie)
+                            .method(Connection.Method.GET)
+                            .execute()
+                    val response = Jsoup.connect("http://compus.uom.gr/modules/anns/anns.php")
+                            .cookie("PHPSESSID",cookie)
+                            .method(Connection.Method.GET)
+                            .execute()
+
+                    val document = response.parse()
+                    val announcements = ArrayList<String>()
+                    val announcementElements = document!!.select("div")
+                    for (i in announcementElements.indices) {
+                        announcements.add(announcementElements[i].text())
+                        Log.i("Announcement",announcementElements[i].text())
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                return@async
+            }.await()
+        }
+
+        loginButton.setOnClickListener {
+            GlobalScope.launch  (Dispatchers.Main) {
+                val username = usrText.text.toString()
+                val password = passwdText.text.toString()
+                if (username.contains("42069"))
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/dQw4w9WgXcQ")))
+
+                val cookie = login(username,password)
+                if (cookie != null) {
+                    AccountManager.get(context)
+                    CookieManager.getInstance().setCookie("http://compus.uom.gr","PHPSESSID=$cookie")
+                    val document = fetchHome(cookie)
+                    if (document != null) {
+                        val courses = showHome(document)
+                        getAnnouncements(cookie, courses)
+                    }
+                } else
+                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+}
